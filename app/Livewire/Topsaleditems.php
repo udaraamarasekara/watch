ef0b72f2bd142efe6f63190c730e0937550dff24
udara <udaraprabath98@gmail.com>
2023-11-11 18:07:58 +0530

    rrr

--- app/Livewire/Topsaleditems.php
+++ app/Livewire/Topsaleditems.php
@@ -2,18 +2,16 @@
 
 namespace App\Livewire;
 use App\Models\Watch;
-use Exception;
 use Livewire\Component;
 
 class Topsaleditems extends Component
 {
     public function render()
-    { try{
+    {
         $topSaleItems =Watch::orderBy('sold','desc')->limit(3)->get();
+        foreach($topSaleItems as $item){
+            !$item->sold && $item->sold=0;
+        }
         return view('livewire.topsaleditems',['topSaleItems'=>$topSaleItems]);
-    }catch(Exception $e){
-      dd($e);
-    } 
-        
     }
 }
